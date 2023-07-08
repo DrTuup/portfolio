ce4d3355ba7b35270bfb9ae7472f367070a02c71
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2023-07-08 19:48:54 +0200

    home page looks like I want

--- app/page.tsx
+++ app/page.tsx
@@ -12,46 +12,44 @@ import Spacer from "@/components/spacer";
 export default function Home() {
   return (
     <>
-      <main>
-        <Navbar />
-        <div className="flex min-h-screen h-fit flex-row items-center justify-center p-50">
-          <div className="flex flex-row items-center gap-60">
-            <div className="flex flex-col justify-center gap-5">
-              <h1 className="text-7xl font-bold">
-                Ruben <br /> Claessens
-              </h1>
-              <p className="text-3xl font-semibold primaryrgb">
-                Full-stack developer 👨🏻‍💻
-              </p>
-              <Spacer />
-              <div className="flex flex-row gap-5">
-                <IconButton
-                  target="_blank"
-                  href="https://github.com/DrTuup"
-                  className="w-fit h-fit rounded foregroundrgb outline outline-2 outline-emerald-300 p-3 hover:scale-110 transition-all duration-300"
-                >
-                  Github &nbsp;
-                  <GitHubIcon fontSize="large" />
-                </IconButton>
-                <IconButton
-                  target="_blank"
-                  href="https://www.linkedin.com/in/ruben-claessens/"
-                  className="w-fit h-fit rounded foregroundrgb outline outline-2 outline-emerald-300 p-3 hover:scale-110 transition-all duration-300"
-                >
-                  LinkedIn &nbsp;
-                  <LinkedIn fontSize="large" />
-                </IconButton>
-              </div>
+      <Navbar />
+      <div className="flex min-h-screen h-fit flex-row items-center justify-center p-50">
+        <div className="flex flex-row items-center gap-60">
+          <div className="flex flex-col justify-center gap-5">
+            <h1 className="text-7xl font-bold">
+              Ruben <br /> Claessens
+            </h1>
+            <p className="text-3xl font-semibold primaryrgb">
+              Full-stack developer 👨🏻‍💻
+            </p>
+            <Spacer />
+            <div className="flex flex-row gap-5">
+              <IconButton
+                target="_blank"
+                href="https://github.com/DrTuup"
+                className="w-fit h-fit rounded foregroundrgb outline outline-2 outline-emerald-300 p-3 hover:scale-110 transition-all duration-300"
+              >
+                Github &nbsp;
+                <GitHubIcon fontSize="large" />
+              </IconButton>
+              <IconButton
+                target="_blank"
+                href="https://www.linkedin.com/in/ruben-claessens/"
+                className="w-fit h-fit rounded foregroundrgb outline outline-2 outline-emerald-300 p-3 hover:scale-110 transition-all duration-300"
+              >
+                LinkedIn &nbsp;
+                <LinkedIn fontSize="large" />
+              </IconButton>
             </div>
-            <Image src={image} alt="memoji" className="scale-75" />
           </div>
-          <p className="absolute bottom-20">Get to know me!</p>
-          <ArrowDownward
-            fontSize="large"
-            className="absolute bottom-5 animate-bounce-arrow scale-150 primaryrgb"
-          />
+          <Image src={image} alt="memoji" className="scale-75" />
         </div>
-      </main>
+        <p className="absolute bottom-20">Get to know me!</p>
+        <ArrowDownward
+          fontSize="large"
+          className="absolute bottom-5 animate-bounce-arrow scale-150 primaryrgb"
+        />
+      </div>
     </>
   );
 }
