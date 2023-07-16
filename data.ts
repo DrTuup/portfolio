c0c10ed2cf7edc1fed297989d6fb63d51163e56f
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2023-07-16 12:53:58 +0200

    added data

---
+++ data.ts
@@ -0,0 +1,33 @@
+export const frontend = [
+  "HTML5",
+  "CSS3",
+  "TailwindCSS",
+  "JavaScript",
+  "NextJS",
+  "Splunk",
+];
+
+export const backend = [
+  "NodeJS",
+  "Python",
+  "JavaScript",
+  ".NET Core",
+  "Splunk",
+  "SQL",
+];
+
+export const tools = [
+  "Visual Studio (Code)",
+  "Git",
+  "GitHub",
+  "Postman",
+  "Jira",
+  "UML",
+];
+
+export const professional = [
+  "Communicating",
+  "Leading",
+  "Problem solving",
+  "Collaborating",
+];
