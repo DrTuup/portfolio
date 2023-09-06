beaada60bc8cfdaca117b309ac05ea0e5ac15310
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2023-09-06 13:36:55 +0200

    not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,25 @@
-import Link from "next/link";
+import Image from "next/image";
+import notfound from "@/assets/images/not-found.png";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <div
-        id="home"
-        className="flex min-h-screen h-fit flex-row items-center justify-center"
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <Image
+        src={notfound}
+        alt="memoji-not-found"
+        className="w-[150px] h-[150px]"
+      />
+
+      <h1 className="text-4xl font-bold text-foreground m-0 p-0">404</h1>
+      <p className="text-lg font-bold text-foreground m-0 p-0">
+        Page Not Found
+      </p>
+      <a
+        href="/"
+        className="text-foreground hover:bg-primary w-fit transition duration-300 p-3 rounded-lg outline outline-primary"
       >
-        <div className="flex flex-row items-center gap-40">
-          <div className="flex flex-col justify-center gap-5">
-            <p className="text-3xl font-semibold text-foreground">
-              Oops, this page doesn&apos;t exist!
-            </p>
-            <Link
-              href="/"
-              className="text-foreground hover:bg-primary w-fit transition duration-300 p-3 rounded-lg outline outline-primary"
-            >
-              Return to home
-            </Link>
-          </div>
-        </div>
-      </div>
-      ;
-    </>
+        Return to home
+      </a>
+    </div>
   );
 }
