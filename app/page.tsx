c89253659d27c2c88c47aab0cf5dace0f0947aa0
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2025-03-14 18:25:47 +0100

    Start on the first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  function calcAge() {
+    const today = new Date();
+    const birthDate = new Date("2002-12-28");
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--;
+    return age;
+  }
+  return (
+    <div className="max-w-screen w-screen min-h-screen flex flex-col p-5 gap-4">
+      <Image src="/memoji.png" width={120} height={120} alt="memoji" />
+      <h1 className="font-bold text-2xl text-primary">
+        Ruben
+        <br />
+        Claessens
+      </h1>
+      <h2 className="font-bold w-fit p-2 rounded-md bg-[var(--chart-1)]">
+        DevOps Engineer
+      </h2>
+      <p className="">
+        A {calcAge()} years old <strong>DevOps Engineer</strong> who loves
+        automating everything and turning ideas into code. <br />
+      </p>
+    </div>
+  );
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { NavItem } from "@/models/navitem";
+import { usePathname } from "next/navigation";
+
+const navitems: NavItem[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Experience",
+    href: "/experience",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
+
+export function Navigation() {
+  const pathname = usePathname();
+  return (
+    <div className="w-full flex justify-center align-center fixed bottom-0">
+      <div className="bg-secondary my-8 mx-4 flex flex-row p-1 rounded-full">
+        {navitems.map((item) => {
+          return (
+            <a
+              className={`text-sm font-bold rounded-full p-2 ${
+                pathname === item.href ? `outline outline-[var(--chart-1)]` : ``
+              }`}
+              key={item.title}
+              href={item.href}
+            >
+              {item.title}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
